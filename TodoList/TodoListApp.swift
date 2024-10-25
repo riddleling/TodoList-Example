f33eece0297aa7a4986f27284abb863f1099ae33
Riddle Ling <riddle.apple@gmail.com>
2024-10-26 00:11:23 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Riddle Ling on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
